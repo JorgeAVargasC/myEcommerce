3ce78f6dee40609556972182ccb817515e451320
JorgeVargas <javargas216@unicauca.edu.co>
2022-06-20 18:54:06 -0500

    AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,15 @@
+import React,{createContext,useReducer} from 'react'
+import authReducer, { initialState } from '../reducers/authReducer'
+
+export const authContext = createContext()
+
+export default function AuthProvider({children}) {
+
+    const [state,dispatch] = useReducer(authReducer,initialState)
+
+    return (
+        <authContext.Provider value={{ user:state.user, logged:state.logged, setUser:dispatch }}>
+            {children}
+        </authContext.Provider>
+    )
+}
