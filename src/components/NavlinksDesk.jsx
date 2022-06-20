864de8f48e4d488a2ac6f2728a812f5ac93d16ab
JorgeVargas <javargas216@unicauca.edu.co>
2022-06-20 15:51:15 -0500

    Hover animation for desktop navlinks

--- src/components/NavlinksDesk.jsx
+++ src/components/NavlinksDesk.jsx
@@ -9,7 +9,7 @@ export default function NavlinksDesk() {
 		if (location.pathname === path) {
 			return `${baseStyle} border-emerald-500 text-emerald-500`;
 		} else {
-			return `${baseStyle} border-slate-900 text-slate-500`;
+			return `${baseStyle} border-slate-900 text-slate-500 hover:border-emerald-500 hover:text-emerald-500 duration-200`;
 		}
 	}
 
@@ -43,7 +43,7 @@ export default function NavlinksDesk() {
 				<li className="flex mr-4 justify-center items-center h-full">
 					<Link
 						to={"/login"}
-						className="w-28 h-2/3 flex justify-center items-center border-2 border-emerald-500 text-emerald-500 rounded"
+						className="w-28 h-2/3 flex justify-center items-center border-2 border-emerald-500 text-emerald-500 rounded hover:bg-emerald-500 hover:text-white duration-200"
 					>
 						Login
 					</Link>
@@ -51,7 +51,7 @@ export default function NavlinksDesk() {
 				<li className="flex justify-center items-center h-full">
 					<Link
 						to={"/signup"}
-						className="w-28 h-2/3 flex justify-center items-center border-2 border-emerald-500 bg-emerald-500 rounded"
+						className="w-28 h-2/3 flex justify-center items-center border-2 border-emerald-500 bg-emerald-500 rounded hover:bg-emerald-600 duration-200 hover:border-emerald-600"
 					>
 						Sign Up
 					</Link>
