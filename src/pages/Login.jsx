70b4fb314b7fafc65b0ddba75b39b9e448ff06a1
JorgeVargas <javargas216@unicauca.edu.co>
2022-06-20 22:11:53 -0500

    Login form v1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,60 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { post } from "../api/";
+import { authContext } from "../context/AuthContext";
+
 export default function Login() {
+	const { setUser } = useContext(authContext);
+	const navigate = useNavigate();
+	const [errors, setErrors] = useState({
+		isError: false,
+		errors: [],
+	});
+
+	// const login = (e) => {
+	// 	e.preventDefault();
+
+	// 	post("/api/auth/login", data).then(({user})=>{
+	// 		setUser({type:'LOGIN', payload:user})
+	// 		navigate("/")
+	// 	})
+
+	// }
+
 	return (
-		<div className="w-11/12 mt-20 bg-slate-700 flex justify-center items-center">
-			<div className="relative">
-				<input
-					type="text"
-					className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-					placeholder=" "
-				/>
-				<label
-					for="floating_outlined"
-					className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-800 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-				>
-					Floating outlined
-				</label>
+		<div className="w-11/12 min-h-screen flex justify-center items-center">
+			<div className="w-11/12 md:w-1/3">
+				<div className="relative mb-4">
+					<input
+						type="text"
+						className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded border-1 border-slate-500 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-500 peer"
+						placeholder=" "
+					/>
+					<label
+						for="floating_outlined"
+						className="absolute text-sm text-slate-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-slate-900 px-2 peer-focus:px-2 peer-focus:text-emerald-500 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+					>
+						Email
+					</label>
+				</div>
+
+				<div className="relative mb-4">
+					<input
+						type="text"
+						className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded border-1 border-slate-500 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-500 peer"
+						placeholder=" "
+					/>
+					<label
+						for="floating_outlined"
+						className="absolute text-sm text-slate-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-slate-900 px-2 peer-focus:px-2 peer-focus:text-emerald-500 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+					>
+						Password
+					</label>
+				</div>
+
+				<button className="bg-emerald-500 border-emerald-500 border-2 rounded flex items-center justify-center w-full h-10 hover:bg-emerald-600 duration-200 hover:border-emerald-600">
+					Login
+				</button>
 			</div>
 		</div>
 	);
