27373398fc86258e70401d9c7908c390e66bba11
JorgeVargas <javargas216@unicauca.edu.co>
2022-06-30 12:27:41 -0500

    table v1 for cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,40 +3,57 @@ import { del, get } from "../api";
 import { cartContext } from "../context/CartContext";
 
 export default function Cart() {
-	const { items, setItems } = useContext(cartContext);
+	const { items, setItems } = useContext(cartContext);	
 
-	del("/api/cart/remove",{
-		idProduct:"62ba16bcb96a7de3835aeed5"
-	})
-	.then(data => {
-		console.log(data)
-	})
-	
-
-	useEffect(() => {
-		get("/api/cart")
-			.then((data) => {
-				console.log(data);
-				setItems({
-					type: "UPDATE",
-					payload: data,
-				});
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, [setItems]);
+	// useEffect(() => {
+	// 	get("/api/cart")
+	// 		.then((data) => {
+	// 			console.log(data);
+	// 			setItems({
+	// 				type: "UPDATE",
+	// 				payload: data,
+	// 			});
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log(error);
+	// 		});
+	// }, [setItems]);
 
 	return (
-		<div className="w-11/12 mt-20 bg-slate-700 flex justify-center items-center flex-col">
+		<div className="w-11/12 mt-20  flex justify-center items-center flex-col">
 			<h1>Cart</h1>
-			{/* <p>Items: {items.length}</p> */}
+			<p>Items: {items.length}</p>
 			<button >
 				Clear Cart
 			</button>
 
-			<table>
+			<table className="w-full bg-emerald-500 rounded text-left">
 				<thead>
+					<tr>
+						<th>Image</th>
+						<th>Name</th>
+						<th>Price</th>
+						<th>Amount</th>
+						<th>Total</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+				{items?.map((item) => {
+					return (
+						<tr key={item._id} className="bg-slate-800">
+							<td><img className="w-8 h-auto" src={item.images[0]} alt={item.name} /></td>
+							<td>{item.name}</td>
+							<td>{item.price}</td>
+							<td>{item.amount}</td>
+							<td>{item.amount * item.price}</td>
+							<td></td>
+						</tr>
+					);
+				})}
+				</tbody>
+
+				{/* <tfoot>
 					<tr>
 						<th>Image</th>
 						<th>Name</th>
@@ -44,21 +61,7 @@ export default function Cart() {
 						<th>Amount</th>
 						<th>Action</th>
 					</tr>
-				</thead>
-				<tbody>
-				{/* {items.map((item, index) => {
-					return (
-						<tr>
-							<td><img className="w-8 h-auto" src={item.images[0]} alt={item.name} /></td>
-							<td>{item.name}</td>
-							<td>{item.price}</td>
-							<td>{item.amount}</td>
-							<td></td>
-						</tr>
-					);
-				})} */}
-				</tbody>
-
+				</tfoot> */}
 				
 			</table>
 		</div>
